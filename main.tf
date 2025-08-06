0a4648d669dccdb851f62a7e8a6e1cdc8e14cd4c
sharmah91 <sharmah91@gmail.com>
2025-08-06 16:40:58 +0530

    block changed

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
   subscription_id = "f96f2e3b-2cc8-4e5e-8ed9-4afb664bb5a8"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rsg" {
   name     = "punitrg"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "stg" {
+resource "azurerm_storage_account" "stpg" {
   name                     = "haramistorage"
   location                 = "East US"
   resource_group_name      = "punitrg"
